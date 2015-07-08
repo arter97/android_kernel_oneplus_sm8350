6e467769af19a9d744b1d080b3349ca85f75d57e
Mayank Rana <mrana@codeaurora.org>
2015-07-08 13:32:15 -0700

    dwc3-msm: Add support to disable host mode PM runtime
    In host mode different issues like PHY is not going into IN_L2 state on
    device disconnect or PORTSC interrupt is not seen on device connect case.
    Hence add support to disable host mode PM runtime which prevent USB host
    bus suspend.
    
    Change-Id: Ic189de889db5368ee91accf4965a1a2866bdb69c
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -570,6 +570,7 @@ struct dwc3_msm {
 	struct device_node	*ss_redriver_node;
 
 	bool			perf_mode;
+	bool			disable_host_mode_pm;
 };
 
 #define USB_HSPHY_3P3_VOL_MIN		3050000 /* uV */
@@ -4770,6 +4771,10 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 
 	mdwc->use_pdc_interrupts = of_property_read_bool(node,
 				"qcom,use-pdc-interrupts");
+
+	mdwc->disable_host_mode_pm = of_property_read_bool(node,
+				"qcom,disable-host-mode-pm");
+
 	dwc3_set_notifier(&dwc3_msm_notify_event);
 
 	if (dma_set_mask_and_coherent(dev, DMA_BIT_MASK(64))) {
@@ -5403,6 +5408,15 @@ static int dwc3_otg_start_host(struct dwc3_msm *mdwc, int on)
 				PM_QOS_CPU_DMA_LATENCY, PM_QOS_DEFAULT_VALUE);
 		/* start in perf mode for better performance initially */
 		msm_dwc3_perf_vote_update(mdwc, true);
+
+		/*
+		 * In some cases it is observed that USB PHY is not going into
+		 * suspend with host mode suspend functionality. Hence disable
+		 * XHCI's runtime PM here if disable_host_mode_pm is set.
+		 */
+		if (mdwc->disable_host_mode_pm)
+			pm_runtime_disable(&dwc->xhci->dev);
+
 		schedule_delayed_work(&mdwc->perf_vote_work,
 				msecs_to_jiffies(1000 * PM_QOS_SAMPLE_SEC));
 	} else {
