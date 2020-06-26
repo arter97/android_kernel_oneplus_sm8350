d2d298c1787fe269bd249a9b01457f41bc68650a
Joonsoo Kim <iamjoonsoo.kim@lge.com>
2020-06-25 20:30:37 -0700

    mm/memory: fix IO cost for anonymous page
    With synchronous IO swap device, swap-in is directly handled in fault
    code.  Since IO cost notation isn't added there, with synchronous IO
    swap device, LRU balancing could be wrongly biased.  Fix it to count it
    in fault code.
    
    Link: http://lkml.kernel.org/r/1592288204-27734-4-git-send-email-iamjoonsoo.kim@lge.com
    Fixes: 314b57fb0460001 ("mm: balance LRU lists based on relative thrashing cache sizing")
    Signed-off-by: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Joonsoo Kim <js1304@gmail.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Minchan Kim <minchan.kim@gmail.com>
    Cc: Rik van Riel <riel@surriel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- mm/memory.c
+++ mm/memory.c
@@ -3090,6 +3090,14 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 				if (err)
 					goto out_page;
 
+				/*
+				 * XXX: Move to lru_cache_add() when it
+				 * supports new vs putback
+				 */
+				spin_lock_irq(&page_pgdat(page)->lru_lock);
+				lru_note_cost_page(page);
+				spin_unlock_irq(&page_pgdat(page)->lru_lock);
+
 				lru_cache_add(page);
 				swap_readpage(page, true);
 			}
