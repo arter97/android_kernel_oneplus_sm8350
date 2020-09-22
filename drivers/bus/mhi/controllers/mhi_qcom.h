f52ab333c6dce22b818f380a1737bcdbf10461d0
Bhaumik Bhatt <bbhatt@codeaurora.org>
2020-09-22 10:25:06 -0700

    mhi: cntrl: qcom: Skip forced suspend if it is not necessary
    In some modem attaches, the forced suspend after mission mode
    entry to switch phy settings may not be necessary. Read the
    device tree property to determine that, and skip it during the
    boot-up phase. Add some comments to explain this and other
    configuration behavior.
    
    Change-Id: I712ead79101c89913c369a689ff7fad72056108f
    Signed-off-by: Bhaumik Bhatt <bbhatt@codeaurora.org>
    Signed-off-by: Mrinmay Sarkar <msarkar@codeaurora.org>

--- drivers/bus/mhi/controllers/mhi_qcom.h
+++ drivers/bus/mhi/controllers/mhi_qcom.h
@@ -60,6 +60,7 @@ struct mhi_dev {
 	int resn;
 	void *arch_info;
 	bool powered_on;
+	bool skip_forced_suspend;
 	dma_addr_t iova_start;
 	dma_addr_t iova_stop;
 	enum mhi_suspend_mode suspend_mode;
