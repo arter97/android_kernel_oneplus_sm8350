e604db3afc3ac27db8d24453646a0432a12bb031
Gustavo A. R. Silva <gustavoars@kernel.org>
2020-11-20 12:32:26 -0600

    selinux: Fix fall-through warnings for Clang
    In preparation to enable -Wimplicit-fallthrough for Clang, fix a warning
    by explicitly adding a break statement instead of letting the code fall
    through to the next case.
    
    Link: https://github.com/KSPP/linux/issues/115
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -4054,6 +4054,7 @@ static int selinux_kernel_load_data(enum kernel_load_data_id id)
 	switch (id) {
 	case LOADING_MODULE:
 		rc = selinux_kernel_module_from_file(NULL);
+		break;
 	default:
 		break;
 	}
