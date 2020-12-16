dae83ca838039093a0db6dad29f81860e9899bf0
Avaneesh Kumar Dwivedi <akdwived@codeaurora.org>
2020-12-16 21:18:35 +0530

    drivers: soc: qcom: Add SLATE PIL driver support
    SLATE is a companion chip for Main SoC for low power needs.
    Add SLATE PIL driver to provide load, subsystem restart
    and ramdump collection support for companion slate chip.
    
    The driver loads the slate firmware into memory and sends
    a command to the secure userspace application to initiate
    secure transfer of the firmware into internal memory
    of SLATE and bring it out of reset. The subsystem restart
    depends on couple of GPIOs present between MSM and SLATE.
    
    Change-Id: I3a97ccdb99a9c744fa902614e7298d87e0155ceb
    Signed-off-by: Avaneesh Kumar Dwivedi <akdwived@codeaurora.org>

---
+++ drivers/soc/qcom/pil_slate_intf.h
@@ -0,0 +1,39 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2021, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef __SLATE_INTF_H_
+#define __SLATE_INTF_H_
+
+#define MAX_APP_NAME_SIZE 100
+#define RESULT_SUCCESS 0
+#define RESULT_FAILURE -1
+
+/* tzapp command list.*/
+enum slate_tz_commands {
+	SLATEPIL_RAMDUMP,
+	SLATEPIL_IMAGE_LOAD,
+	SLATEPIL_AUTH_MDT,
+	SLATEPIL_DLOAD_CONT,
+	SLATEPIL_GET_SLATE_VERSION,
+	SLATEPIL_SHUTDOWN,
+};
+
+/* tzapp bg request.*/
+struct tzapp_slate_req {
+	uint8_t tzapp_slate_cmd;
+	uint8_t padding[3];
+	phys_addr_t address_fw;
+	size_t size_fw;
+} __attribute__ ((__packed__));
+
+/* tzapp bg response.*/
+struct tzapp_slate_rsp {
+	uint32_t tzapp_slate_cmd;
+	uint32_t slate_info_len;
+	int32_t status;
+	uint32_t slate_info[100];
+} __attribute__ ((__packed__));
+
+#endif
