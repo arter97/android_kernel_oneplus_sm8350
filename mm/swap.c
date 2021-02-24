d8764a83a5cfad2119c6c1567384b5c083bec59c
Yu Zhao <yuzhao@google.com>
2021-02-24 12:08:21 -0800

    mm/swap.c: don't pass "enum lru_list" to trace_mm_lru_insertion()
    The parameter is redundant in the sense that it can be extracted
    from the "struct page" parameter by page_lru() correctly.
    
    Link: https://lore.kernel.org/linux-mm/20201207220949.830352-5-yuzhao@google.com/
    Link: https://lkml.kernel.org/r/20210122220600.906146-5-yuzhao@google.com
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Reviewed-by: Alex Shi <alex.shi@linux.alibaba.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Roman Gushchin <guro@fb.com>
    Cc: Vladimir Davydov <vdavydov.dev@gmail.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 861404536a3af3c39f1b10959a40def3d8efa2dd)
    Bug: 227651406
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>
    Change-Id: Ia02c0c65dd427a98ffa39e9dc3e2ae701e85fad8
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- mm/swap.c
+++ mm/swap.c
@@ -951,7 +951,6 @@ void lru_add_page_tail(struct page *page, struct page *page_tail,
 static void __pagevec_lru_add_fn(struct page *page, struct lruvec *lruvec,
 				 void *arg)
 {
-	enum lru_list lru;
 	int was_unevictable = TestClearPageUnevictable(page);
 
 	VM_BUG_ON_PAGE(PageLRU(page), page);
@@ -986,11 +985,9 @@ static void __pagevec_lru_add_fn(struct page *page, struct lruvec *lruvec,
 	smp_mb();
 
 	if (page_evictable(page)) {
-		lru = page_lru(page);
 		if (was_unevictable)
 			count_vm_event(UNEVICTABLE_PGRESCUED);
 	} else {
-		lru = LRU_UNEVICTABLE;
 		ClearPageActive(page);
 		SetPageUnevictable(page);
 		if (!was_unevictable)
@@ -998,7 +995,7 @@ static void __pagevec_lru_add_fn(struct page *page, struct lruvec *lruvec,
 	}
 
 	add_page_to_lru_list(page, lruvec);
-	trace_mm_lru_insertion(page, lru);
+	trace_mm_lru_insertion(page);
 }
 
 /*
