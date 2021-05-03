b63df46a2297b6678c163987490cab6c816b6dad
Kristof Petho <kristof.petho@gmail.com>
2021-05-03 07:32:55 +0200

    Revert: disp: msm: enable event log and disp ramdump without DEBUG_FS
    Let's not compile all this debugging in if not really needed

--- techpack/display/msm/sde_dbg.c
+++ techpack/display/msm/sde_dbg.c
@@ -1294,7 +1294,6 @@ void sde_dbg_ctrl(const char *name, ...)
 	va_end(args);
 }
 
-#ifdef CONFIG_DEBUG_FS
 /*
  * sde_dbg_debugfs_open - debugfs open handler for evtlog dump
  * @inode: debugfs inode
@@ -2284,15 +2283,6 @@ int sde_dbg_debugfs_register(struct device *dev)
 
 	return 0;
 }
-
-#else
-
-int sde_dbg_debugfs_register(struct device *dev)
-{
-	return 0;
-}
-
-#endif
 
 static void _sde_dbg_debugfs_destroy(void)
 {
