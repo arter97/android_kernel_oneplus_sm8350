f3f1cb5786c6c5bd1c4c6bcd173f301ba40b79b1
Santosh Dronamraju <sdronamr@codeaurora.org>
2021-06-21 21:34:07 +0530

    crypto-qti-tz: Add derive raw secret functionality
    Add support for the derive raw secret function call. With
    this scm call will use secure world to generate secret.
    
    TZ will not maintain the file name key. only a secret
    will be derived from secure world to further derive file
    name key from it.
    
    Test: vts-kernel-encryption test.
    Change-Id: I001506d37fb16bfc29d89ac74232018419e66d44
    Signed-off-by: Santosh Dronamraju <sdronamr@codeaurora.org>

--- drivers/soc/qcom/crypto-qti-common.c
+++ drivers/soc/qcom/crypto-qti-common.c
@@ -484,10 +484,16 @@ int crypto_qti_derive_raw_secret(void *priv_data,
 		err = -EINVAL;
 		return err;
 	}
+	if (wrapped_key_size > 64)
+		err = crypto_qti_derive_raw_secret_platform(ice_entry,
+							    wrapped_key,
+							    wrapped_key_size,
+							    secret,
+							    secret_size);
+	else
+		memcpy(secret, wrapped_key, secret_size);
 
-	return crypto_qti_derive_raw_secret_platform(ice_entry,
-				wrapped_key, wrapped_key_size,
-				secret, secret_size);
+	return err;
 }
 EXPORT_SYMBOL(crypto_qti_derive_raw_secret);
 
