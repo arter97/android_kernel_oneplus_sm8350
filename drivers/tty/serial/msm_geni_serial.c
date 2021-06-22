f586511914d55176dcf15b18007620bcc2e8f580
Aniket Randive <arandive@codeaurora.org>
2021-06-22 17:31:49 +0530

    serial: msm_gen_serial: Balance the usage count for UART GSI
    Due to the start tx is called multiple time, the usage count
    is incremented everytime as part of start tx. so this will
    balance out the power vote put in during start_tx.
    
    Change-Id: I038ea1bea17dc8dcd2926b20b41953e1cf43a04e
    Signed-off-by: Aniket Randive <arandive@codeaurora.org>

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -1299,9 +1299,23 @@ static void msm_geni_uart_gsi_tx_cb(void *ptr)
 	msm_port->tx_dma = (dma_addr_t)NULL;
 	msm_port->xmit_size = 0;
 	complete(&msm_port->tx_xfer);
-	queue_work(msm_port->tx_wq, &msm_port->tx_xfer_work);
+	if (!uart_circ_empty(xmit)) {
+		queue_work(msm_port->tx_wq, &msm_port->tx_xfer_work);
+		IPC_LOG_MSG(msm_port->ipc_log_misc, "%s: TX_xfer is Queued\n",
+				__func__);
+	} else {
+		/*
+		 * This will balance out the power vote put in during start_tx
+		 * allowing the device to suspend.
+		 */
+		if (!uart_console(uport)) {
+			IPC_LOG_MSG(msm_port->ipc_log_misc,
+				"%s.Tx sent out, Power off\n", __func__);
+			msm_geni_serial_power_off(uport);
+		}
+		uart_write_wakeup(uport);
+	}
 	IPC_LOG_MSG(msm_port->ipc_log_misc, "%s:End\n", __func__);
-
 }
 
 static void msm_geni_uart_rx_queue_dma_tre(int index, struct uart_port *uport)
