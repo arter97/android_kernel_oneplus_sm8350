1895585325fcb9a6fde582b9da9b021646c86e5a
Juhyung Park <qkrwngud825@gmail.com>
2021-08-14 01:04:26 +0900

    ramdisk: fix permission scripts
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- ramdisk_fix_permissions.sh
+++ ramdisk_fix_permissions.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+chmod 555 config
 chmod 644 file_contexts
 chmod 644 se*
 chmod 644 *.sh
@@ -7,6 +8,7 @@ chmod 750 init*
 chmod 640 fstab*
 chmod 600 default.prop prop.default
 chmod 771 data
+chmod 777 sdcard
 chmod 755 dev
 chmod 755 proc
 chmod 755 bin
@@ -16,3 +18,5 @@ chmod 750 sbin/*
 chmod 751 storage
 chmod 755 sys
 chmod 755 system
+chmod 751 system/bin
+chmod 550 system/bin/logd
