363b2be8fc130406f8aa69d4772077b771a6d941
Juhyung Park <qkrwngud825@gmail.com>
2021-08-18 20:42:38 +0900

    qcacld: enable read_mac_addr_from_mac_file
    The stock kernel module hardcodes this in.
    
    Without this, the device generates random MAC address which may randomly cause
    failure in Wi-Fi device probing.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- drivers/staging/qcacld-3.0/core/hdd/inc/hdd_config.h
+++ drivers/staging/qcacld-3.0/core/hdd/inc/hdd_config.h
@@ -776,7 +776,7 @@ struct dhcp_server {
  * read_mac_addr_from_mac_file - Use/ignore MAC address from mac cfg file
  * @Min: 0
  * @Max: 1
- * @Default: 0
+ * @Default: 1
  *
  * This ini is used whether to configure MAC address from the cfg file or not
  *
@@ -788,7 +788,7 @@ struct dhcp_server {
  */
 #define CFG_READ_MAC_ADDR_FROM_MAC_FILE CFG_INI_BOOL( \
 	"read_mac_addr_from_mac_file", \
-	0, \
+	1, \
 	"Use/ignore MAC address from cfg file")
 
 /*
