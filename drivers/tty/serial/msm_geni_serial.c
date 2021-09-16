0cb1505ea4cebfd9d79eb8f2da49f855efbde50c
Aniket Randive <arandive@codeaurora.org>
2021-09-16 15:53:30 +0530

    Serial: msm_geni_serial: Remove the vote in GSI mode
    Currently for GSI mode, we are not removing the vote at the end
    of stop tx sequencer and due to this if we cancel an on-going Tx
    transfer then there is vote imbalance because we put the vote at
    start of TX transfer and removing a vote at the end of TX transfer.
    Due to the vote being active, LPM test will fail. To avoid it,
    remove the vote.
    
    Change-Id: I8b5d621bf0cde5b9f3eb579d6ffa70be0603f4d9
    Signed-off-by: Aniket Randive <arandive@codeaurora.org>

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -1798,7 +1798,7 @@ static void stop_tx_sequencer(struct uart_port *uport)
 			atomic_set(&port->xfer_inprogress, 0);
 		}
 		dmaengine_terminate_all(port->gsi->tx_c);
-		return;
+		goto out;
 	}
 	geni_status = geni_read_reg_nolog(uport->membase, SE_GENI_STATUS);
 	/* Possible stop tx is called multiple times. */
@@ -1874,6 +1874,7 @@ static void stop_tx_sequencer(struct uart_port *uport)
 	port->m_cmd = false;
 	port->xmit_size = 0;
 
+out:
 	/*
 	 * If we end up having to cancel an on-going Tx for non-console usecase
 	 * then it means there was some unsent data in the Tx FIFO, consequently
