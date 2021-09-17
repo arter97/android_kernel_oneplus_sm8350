a309c93a81b355b27e362b5095f8306537efa1cc
Juhyung Park <qkrwngud825@gmail.com>
2021-09-17 19:10:02 +0900

    time: move frequently used functions to headers and declare them inline
    Those function are frequently used in various places and declaring them inline
    can reduce overheads.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- include/linux/time32.h
+++ include/linux/time32.h
@@ -144,11 +144,27 @@ static inline s64 timespec_to_ns(const struct timespec *ts)
 
 /**
  * ns_to_timespec - Convert nanoseconds to timespec
- * @nsec:	the nanoseconds value to be converted
+ * @nsec:       the nanoseconds value to be converted
  *
  * Returns the timespec representation of the nsec parameter.
  */
-extern struct timespec ns_to_timespec(const s64 nsec);
+static inline struct timespec ns_to_timespec(const s64 nsec)
+{
+	struct timespec ts;
+	s32 rem;
+
+	if (!nsec)
+		return (struct timespec) {0, 0};
+
+	ts.tv_sec = div_s64_rem(nsec, NSEC_PER_SEC, &rem);
+	if (unlikely(rem < 0)) {
+		ts.tv_sec--;
+		rem += NSEC_PER_SEC;
+	}
+	ts.tv_nsec = rem;
+
+	return ts;
+}
 
 /**
  * timespec_add_ns - Adds nanoseconds to a timespec
@@ -200,12 +216,31 @@ static inline s64 timeval_to_ns(const struct timeval *tv)
 
 /**
  * ns_to_timeval - Convert nanoseconds to timeval
- * @nsec:	the nanoseconds value to be converted
+ * @nsec:       the nanoseconds value to be converted
  *
  * Returns the timeval representation of the nsec parameter.
  */
-extern struct timeval ns_to_timeval(const s64 nsec);
-extern struct __kernel_old_timeval ns_to_kernel_old_timeval(s64 nsec);
+static inline struct timeval ns_to_timeval(const s64 nsec)
+{
+	struct timespec ts = ns_to_timespec(nsec);
+	struct timeval tv;
+
+	tv.tv_sec = ts.tv_sec;
+	tv.tv_usec = (suseconds_t) ts.tv_nsec / 1000;
+
+	return tv;
+}
+
+static inline struct __kernel_old_timeval ns_to_kernel_old_timeval(const s64 nsec)
+{
+	struct timespec64 ts = ns_to_timespec64(nsec);
+	struct __kernel_old_timeval tv;
+
+	tv.tv_sec = ts.tv_sec;
+	tv.tv_usec = (suseconds_t)ts.tv_nsec / 1000;
+
+	return tv;
+}
 
 /*
  * Old names for the 32-bit time_t interfaces, these will be removed
