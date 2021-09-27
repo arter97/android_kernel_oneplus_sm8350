451dfd3d1825d1d5e457c65337d8ddfd33fbd1e1
Juhyung Park <qkrwngud825@gmail.com>
2021-09-27 17:20:19 +0900

    Revert "selinux: fix error return code in policydb_read()"
    This reverts commit b2083327ea3ca883735470a28680d9b24ec10dba.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- security/selinux/ss/policydb.c
+++ security/selinux/ss/policydb.c
@@ -2505,7 +2505,6 @@ int policydb_read(struct policydb *p, void *fp)
 	if (rc)
 		goto bad;
 
-	rc = -ENOMEM;
 	p->type_attr_map_array = kvcalloc(p->p_types.nprim,
 					  sizeof(*p->type_attr_map_array),
 					  GFP_KERNEL);
