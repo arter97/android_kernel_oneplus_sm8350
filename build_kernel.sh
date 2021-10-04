00d2d6bb73f55c36bc492b73f8c18d2fc051db18
Juhyung Park <qkrwngud825@gmail.com>
2021-10-04 19:38:49 +0900

    build_kernel: use 2021-09 for the patch level
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,7 +4,7 @@ export RAMFS_SOURCE=`readlink -f $KERNELDIR/ramdisk`
 export PARTITION_SIZE=134217728
 
 export OS="11.0.0"
-export SPL="2021-07"
+export SPL="2021-09"
 
 echo "kerneldir = $KERNELDIR"
 echo "ramfs_source = $RAMFS_SOURCE"
