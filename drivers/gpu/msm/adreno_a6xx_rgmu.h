e782d6995761d1279b10805109c23ced5b6c43be
Hareesh Gundu <hareeshg@codeaurora.org>
2021-11-11 13:02:21 +0530

    msm: kgsl: Fix GPU GX on check for nogmu targets
    GPU GX will be treated ON when the gfx clock and gxgdsc is
    enabled. For noGMU based targets this is always true under
    the assumptions both are enabled. Fix this by checking the
    gfx clock and gxgdsc status for nogmu targets.
    
    Change-Id: I565e3ac7e13179da89aea122b0cc21078192fe87
    Signed-off-by: Hareesh Gundu <hareeshg@codeaurora.org>

--- drivers/gpu/msm/adreno_a6xx_rgmu.h
+++ drivers/gpu/msm/adreno_a6xx_rgmu.h
@@ -91,4 +91,13 @@ int a6xx_rgmu_restart(struct kgsl_device *device);
  */
 void a6xx_rgmu_snapshot(struct adreno_device *adreno_dev,
 	struct kgsl_snapshot *snapshot);
+
+/**
+ * a6xx_rgmu_gx_is_on() - Check if GX is on using pwr status register
+ * @adreno_dev: Pointer to the adreno device
+ *
+ * This check should only be performed if the keepalive bit is set or it
+ * can be guaranteed that the power state of the GPU will remain unchanged
+ */
+bool a6xx_rgmu_gx_is_on(struct adreno_device *adreno_dev);
 #endif
