57311a93b4a6d4fb9faaae41a62ab665df3c1ae0
Juhyung Park <qkrwngud825@gmail.com>
2021-12-06 19:05:36 +0900

    build_kernel: use 2021-11 for the patch level
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,7 +4,7 @@ export RAMFS_SOURCE=`readlink -f $KERNELDIR/ramdisk`
 export PARTITION_SIZE=134217728
 
 export OS="11.0.0"
-export SPL="2021-09"
+export SPL="2021-11"
 
 echo "kerneldir = $KERNELDIR"
 echo "ramfs_source = $RAMFS_SOURCE"
