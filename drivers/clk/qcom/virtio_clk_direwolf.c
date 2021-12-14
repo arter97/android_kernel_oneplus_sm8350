e1b11bd1d363af5cec521ef7f29c17ee68a545ec
Ramachandran Venkataramani <quic_ramavenk@quicinc.com>
2021-12-13 16:30:45 -0800

    clk: qcom: Add additional USB clocks for Direwolf VM
    Add additional clocks to support USB superspeed on Direwolf VM.
    
    Change-Id: I2907e88d200fddc58f1c51f1df21961b01826447
    Signed-off-by: Ramachandran Venkataramani <quic_ramavenk@quicinc.com>

--- drivers/clk/qcom/virtio_clk_direwolf.c
+++ drivers/clk/qcom/virtio_clk_direwolf.c
@@ -38,6 +38,30 @@ static const char * const dirwolf_gcc_parent_names_4[] = {
 	"bi_tcxo",
 };
 
+static const char * const dirwolf_gcc_parent_names_usb_prim[] = {
+	"usb3_phy_wrapper_gcc_usb30_pipe_clk",
+	"core_bi_pll_test_se",
+	"bi_tcxo",
+};
+
+static const char * const dirwolf_gcc_parent_names_usb_sec[] = {
+	"usb3_uni_phy_sec_gcc_usb30_pipe_clk",
+	"core_bi_pll_test_se",
+	"bi_tcxo",
+};
+
+static const char * const dirwolf_gcc_parent_names_usb_mp0[] = {
+	"usb3_uni_phy_mp_gcc_usb30_pipe_0_clk",
+	"core_bi_pll_test_se",
+	"bi_tcxo",
+};
+
+static const char * const dirwolf_gcc_parent_names_usb_mp1[] = {
+	"usb3_uni_phy_mp_gcc_usb30_pipe_1_clk",
+	"core_bi_pll_test_se",
+	"bi_tcxo",
+};
+
 static const struct virtio_clk_init_data direwolf_gcc_virtio_clocks[] = {
 	[GCC_QUPV3_WRAP0_S0_CLK] = {.name = "gcc_qupv3_wrap0_s0_clk",},
 	[GCC_QUPV3_WRAP0_S1_CLK] = {.name = "gcc_qupv3_wrap0_s1_clk",},
@@ -76,7 +100,13 @@ static const struct virtio_clk_init_data direwolf_gcc_virtio_clocks[] = {
 	[GCC_USB30_PRIM_SLEEP_CLK] = {.name = "gcc_usb30_prim_sleep_clk",},
 	[GCC_USB3_PRIM_PHY_AUX_CLK] = {.name = "gcc_usb3_prim_phy_aux_clk",},
 	[GCC_USB3_PRIM_PHY_PIPE_CLK] = {.name = "gcc_usb3_prim_phy_pipe_clk",},
+	[GCC_USB3_PRIM_PHY_PIPE_CLK_SRC] = {
+				.name = "gcc_usb3_prim_phy_pipe_clk_src",
+				.parent_names = dirwolf_gcc_parent_names_usb_prim,
+				.num_parents = ARRAY_SIZE(dirwolf_gcc_parent_names_usb_prim),
+				},
 	[GCC_USB3_PRIM_PHY_COM_AUX_CLK] = {.name = "gcc_usb3_prim_phy_com_aux_clk",},
+	[GCC_USB4_EUD_CLKREF_CLK] = {.name = "gcc_usb4_eud_clkref_en",},
 	[GCC_USB30_SEC_MASTER_CLK] = {.name = "gcc_usb30_sec_master_clk",},
 	[GCC_CFG_NOC_USB3_SEC_AXI_CLK] = {.name = "gcc_cfg_noc_usb3_sec_axi_clk",},
 	[GCC_AGGRE_USB3_SEC_AXI_CLK] = {.name = "gcc_aggre_usb3_sec_axi_clk",},
@@ -84,7 +114,13 @@ static const struct virtio_clk_init_data direwolf_gcc_virtio_clocks[] = {
 	[GCC_USB30_SEC_SLEEP_CLK] = {.name = "gcc_usb30_sec_sleep_clk",},
 	[GCC_USB3_SEC_PHY_AUX_CLK] = {.name = "gcc_usb3_sec_phy_aux_clk",},
 	[GCC_USB3_SEC_PHY_PIPE_CLK] = {.name = "gcc_usb3_sec_phy_pipe_clk",},
+	[GCC_USB3_SEC_PHY_PIPE_CLK_SRC] = {
+				.name = "gcc_usb3_sec_phy_pipe_clk_src",
+				.parent_names = dirwolf_gcc_parent_names_usb_sec,
+				.num_parents = ARRAY_SIZE(dirwolf_gcc_parent_names_usb_sec),
+				},
 	[GCC_USB3_SEC_PHY_COM_AUX_CLK] = {.name = "gcc_usb3_sec_phy_com_aux_clk",},
+	[GCC_USB4_CLKREF_CLK] = {.name = "gcc_usb4_clkref_en",},
 	[GCC_USB30_MP_MASTER_CLK] = {.name = "gcc_usb30_mp_master_clk",},
 	[GCC_CFG_NOC_USB3_MP_AXI_CLK] = {.name = "gcc_cfg_noc_usb3_mp_axi_clk",},
 	[GCC_AGGRE_USB3_MP_AXI_CLK] = {.name = "gcc_aggre_usb3_mp_axi_clk",},
@@ -98,6 +134,22 @@ static const struct virtio_clk_init_data direwolf_gcc_virtio_clocks[] = {
 	[GCC_USB2_HS1_CLKREF_CLK] = {.name = "gcc_usb2_hs1_clkref_en",},
 	[GCC_USB2_HS2_CLKREF_CLK] = {.name = "gcc_usb2_hs2_clkref_en",},
 	[GCC_USB2_HS3_CLKREF_CLK] = {.name = "gcc_usb2_hs3_clkref_en",},
+	[GCC_USB3_MP_PHY_AUX_CLK] = {.name = "gcc_usb3_mp_phy_aux_clk",},
+	[GCC_USB3_MP_PHY_PIPE_0_CLK] = {.name = "gcc_usb3_mp_phy_pipe_0_clk",},
+	[GCC_USB3_MP_PHY_PIPE_0_CLK_SRC] = {
+				.name = "gcc_usb3_mp_phy_pipe_0_clk_src",
+				.parent_names = dirwolf_gcc_parent_names_usb_mp0,
+				.num_parents = ARRAY_SIZE(dirwolf_gcc_parent_names_usb_mp0),
+				},
+	[GCC_USB3_MP0_CLKREF_CLK] = {.name = "gcc_usb3_mp0_clkref_en",},
+	[GCC_USB3_MP_PHY_COM_AUX_CLK] = {.name = "gcc_usb3_mp_phy_com_aux_clk",},
+	[GCC_USB3_MP_PHY_PIPE_1_CLK] = {.name = "gcc_usb3_mp_phy_pipe_1_clk",},
+	[GCC_USB3_MP_PHY_PIPE_1_CLK_SRC] = {
+				.name = "gcc_usb3_mp_phy_pipe_1_clk_src",
+				.parent_names = dirwolf_gcc_parent_names_usb_mp1,
+				.num_parents = ARRAY_SIZE(dirwolf_gcc_parent_names_usb_mp1),
+				},
+	[GCC_USB3_MP1_CLKREF_CLK] = {.name = "gcc_usb3_mp1_clkref_en",},
 	[GCC_SDCC2_AHB_CLK] = {.name = "gcc_sdcc2_ahb_clk",},
 	[GCC_SDCC2_APPS_CLK] = {.name = "gcc_sdcc2_apps_clk",},
 	[GCC_PCIE_2A_PIPE_CLK] = { .name = "gcc_pcie_2a_pipe_clk",},
@@ -200,6 +252,14 @@ static const char * const direwolf_gcc_virtio_resets[] = {
 	[GCC_QUSB2PHY_HS1_MP_BCR] = "gcc_qusb2phy_hs1_mp_bcr",
 	[GCC_QUSB2PHY_HS2_MP_BCR] = "gcc_qusb2phy_hs2_mp_bcr",
 	[GCC_QUSB2PHY_HS3_MP_BCR] = "gcc_qusb2phy_hs3_mp_bcr",
+	[GCC_USB4_DP_PHY_PRIM_BCR] = "gcc_usb4_dp_phy_prim_bcr",
+	[GCC_USB3_PHY_PRIM_BCR] = "gcc_usb3_phy_prim_bcr",
+	[GCC_USB4_1_DP_PHY_PRIM_BCR] = "gcc_usb4_1_dp_phy_prim_bcr",
+	[GCC_USB3_PHY_SEC_BCR] = "gcc_usb3_phy_sec_bcr",
+	[GCC_USB3_UNIPHY_MP0_BCR] = "gcc_usb3_uniphy_mp0_bcr",
+	[GCC_USB3UNIPHY_PHY_MP0_BCR] = "gcc_usb3uniphy_phy_mp0_bcr",
+	[GCC_USB3_UNIPHY_MP1_BCR] = "gcc_usb3_uniphy_mp1_bcr",
+	[GCC_USB3UNIPHY_PHY_MP1_BCR] = "gcc_usb3uniphy_phy_mp1_bcr",
 	[GCC_PCIE_2A_BCR] = "gcc_pcie_2a_bcr",
 	[GCC_PCIE_2A_PHY_BCR] = "gcc_pcie_2a_phy_bcr",
 	[GCC_PCIE_2A_PHY_NOCSR_COM_PHY_BCR] = "gcc_pcie_2a_phy_nocsr_com_phy_bcr",
