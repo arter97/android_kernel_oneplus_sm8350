75e27be3e0f9360af990d3fe25b7366164eca653
Vinay Sudra <quic_vsudra@quicinc.com>
2021-12-21 11:50:52 +0530

    soc: qcom: early_service: Mount Firmware partition early
    Mount modem partition having firmwares in kernel.
    
    Change-Id: I9cc80fbc49ff5b7b750b2500534b77dd3cc7ba95
    Signed-off-by: Vinay Sudra <quic_vsudra@quicinc.com>

--- drivers/soc/qcom/auto_early_launcher.c
+++ drivers/soc/qcom/auto_early_launcher.c
@@ -4,8 +4,48 @@
  */
 
 #include <linux/module.h>
-#include <linux/mount.h>
+#include <uapi/linux/mount.h>
 #include <linux/syscalls.h>
+
+#define FIRMWARE_MOUNT_PATH	"/early_services/vendor/firmware_mnt"
+
+static char saved_modem_name[64] __initdata;
+
+static int mount_partition(char *part_name, char *mnt_point)
+{
+	int err = -ENOENT;
+	int mountflags = MS_RDONLY | MS_SILENT;
+	const char *fs = "vfat";
+
+	if (!part_name[0]) {
+		pr_err("Unknown partition\n");
+		goto fail;
+	}
+
+	err = ksys_access((const char __user *) mnt_point, 0);
+	if (err == 0)
+		pr_info("early init : mount path  '%s' available\n", mnt_point);
+	else {
+		pr_err("early init : mount path '%s' not available!!\n", mnt_point);
+		goto fail;
+	}
+
+	err = ksys_access((const char __user *) part_name, 0);
+	if (err  == 0)
+		pr_info("early init : node %s available\n", part_name);
+	else {
+		pr_err("early init : node %s not available, error : %d\n", part_name, err);
+		goto fail;
+	}
+	err = ksys_mount((char __user *)part_name, (char __user *)mnt_point,
+			(char __user *)fs,  (unsigned long)mountflags,
+			(void __user *)NULL);
+	if (err)
+		pr_err("Mount Partition [%s] failed, error [%d]\n", part_name, err);
+
+fail:
+	return err;
+}
 
 static int __init early_init(void)
 {
@@ -21,6 +61,14 @@ static int __init early_init(void)
 
 	devtmpfs_mount("dev");
 
+	if (saved_modem_name[0] != '\0') {
+		if (!mount_partition(saved_modem_name, FIRMWARE_MOUNT_PATH))
+			pr_info("Firmwares partition ready\n");
+		else
+			pr_err("Firmwares partition mount fail!\n");
+	} else
+		pr_err("Firmware partition not found!\n");
+
 	if (ksys_access((const char __user *) init_prog, 0) == 0) {
 		ret = call_usermodehelper(init_prog, init_prog_argv, init_envp, 0);
 		if (!ret)
@@ -34,7 +82,14 @@ static int __init early_init(void)
 	return ret;
 }
 
-late_initcall(early_init);
+static int __init modem_dev_setup(char *line)
+{
+	strlcpy(saved_modem_name, line, sizeof(saved_modem_name));
+	return 1;
+}
+__setup("modem=", modem_dev_setup);
+
+late_initcall_sync(early_init);
 
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("Early user space launch driver");
