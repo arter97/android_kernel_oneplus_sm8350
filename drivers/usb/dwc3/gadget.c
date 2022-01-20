e93041e5fd7bf19d4957a76fd044b98591d0f8cb
Ajay Agarwal <quic_ajaya@quicinc.com>
2022-01-20 20:57:11 +0530

    Revert "USB: dwc3: remove debugfs root dentry storage"
    Commit 6b1720c9a6cf ("USB: dwc3: remove debugfs root dentry storage") ended
    up creating the endpoint debugfs directories under debugfs root directory.
    Revert it so that these entries can be created inside dwc->dev folder.
    
    Change-Id: I58cc5844c88f60090c6b69d57c63f76416b2db02
    Signed-off-by: Ajay Agarwal <quic_ajaya@quicinc.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -3142,9 +3142,7 @@ static void dwc3_gadget_free_endpoints(struct dwc3 *dwc)
 			list_del(&dep->endpoint.ep_list);
 		}
 
-		debugfs_remove_recursive(debugfs_lookup(dep->name,
-				debugfs_lookup(dev_name(dep->dwc->dev),
-					       usb_debug_root)));
+		debugfs_remove_recursive(debugfs_lookup(dep->name, dwc->root));
 		kfree(dep);
 	}
 }
