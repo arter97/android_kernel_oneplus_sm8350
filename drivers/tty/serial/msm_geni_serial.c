74fe89e05826c66bdadbbf8a70b17dc19eaf7d33
Aniket Randive <quic_arandive@quicinc.com>
2022-02-21 15:20:43 +0530

    serial: msm_geni_serial: Disable the Rx FIFO WM interrupt
    RX FIFO Watermark interrupt is not required for GSI mode,
    hence disable this interrupt only for GSI mode.
    
    Change-Id: Ifd67fdc4fabb86f06b74a5bb69033e29a8863767
    Signed-off-by: Aniket Randive <quic_arandive@quicinc.com>

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -383,6 +383,11 @@ static void msm_geni_serial_enable_interrupts(struct uart_port *uport)
 	geni_m_irq_en |= M_IRQ_BITS;
 	geni_s_irq_en |= S_IRQ_BITS;
 
+	if (port->gsi_mode) {
+		geni_m_irq_en &= ~M_RX_FIFO_WATERMARK_EN;
+		geni_s_irq_en &= ~S_RX_FIFO_WATERMARK_EN;
+	}
+
 	geni_write_reg_nolog(geni_m_irq_en, uport->membase, SE_GENI_M_IRQ_EN);
 	geni_write_reg_nolog(geni_s_irq_en, uport->membase, SE_GENI_S_IRQ_EN);
 	if (port->xfer_mode == SE_DMA) {
