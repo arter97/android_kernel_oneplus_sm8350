73c29654854b807b39e7262f0ad45f880b85400e
Veera Vegivada <quic_vvegivad@quicinc.com>
2022-03-14 17:00:34 +0530

    icc: dt-bindings: add endpoint IDs for interconnects for SDX55
    Add master and slave ID constants for all Qualcomm Technologies, Inc.
    SDX55 interconnect providers which consumers can use to set bandwidth
    constraints and find paths in the NoC (Network-On-Chip) topology.
    
    Change-Id: If3302c6f1b3c7a03c2801b42d359cf60ba317ece
    Signed-off-by: Veera Vegivada <quic_vvegivad@quicinc.com>

---
+++ include/dt-bindings/interconnect/qcom,sdx55.h
@@ -0,0 +1,70 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#ifndef __DT_BINDINGS_INTERCONNECT_QCOM_SDX55_H
+#define __DT_BINDINGS_INTERCONNECT_QCOM_SDX55_H
+
+#define MASTER_TCU_0				0
+#define MASTER_IPA_CORE				1
+#define MASTER_LLCC				2
+#define MASTER_AUDIO				3
+#define MASTER_BLSP_1				4
+#define MASTER_QDSS_BAM				5
+#define MASTER_QPIC				6
+#define MASTER_SNOC_CFG				7
+#define MASTER_SPMI_FETCHER				8
+#define MASTER_ANOC_SNOC				9
+#define MASTER_IPA				10
+#define MASTER_MEM_NOC_SNOC				11
+#define MASTER_MEM_NOC_PCIE_SNOC				12
+#define MASTER_SNOC_GC_MEM_NOC				13
+#define MASTER_CRYPTO				14
+#define MASTER_APPSS_PROC				15
+#define MASTER_EMAC				16
+#define MASTER_IPA_PCIE				17
+#define MASTER_PCIE_0				18
+#define MASTER_QDSS_ETR				19
+#define MASTER_SDCC_1				20
+#define MASTER_USB3				21
+#define SLAVE_EBI1				512
+#define SLAVE_IPA_CORE				513
+#define SLAVE_AOP				514
+#define SLAVE_AOSS				515
+#define SLAVE_APPSS				516
+#define SLAVE_AUDIO				517
+#define SLAVE_BLSP_1				518
+#define SLAVE_CLK_CTL				519
+#define SLAVE_CRYPTO_0_CFG				520
+#define SLAVE_CNOC_DDRSS				521
+#define SLAVE_ECC_CFG				522
+#define SLAVE_EMAC_CFG				523
+#define SLAVE_IMEM_CFG				524
+#define SLAVE_IPA_CFG				525
+#define SLAVE_CNOC_MSS				526
+#define SLAVE_PCIE_PARF				527
+#define SLAVE_PDM				528
+#define SLAVE_PRNG				529
+#define SLAVE_QDSS_CFG				530
+#define SLAVE_QPIC				531
+#define SLAVE_SDCC_1				532
+#define SLAVE_SNOC_CFG				533
+#define SLAVE_SPMI_FETCHER				534
+#define SLAVE_SPMI_VGI_COEX				535
+#define SLAVE_TCSR				536
+#define SLAVE_TLMM				537
+#define SLAVE_USB3				538
+#define SLAVE_USB3_PHY_CFG				539
+#define SLAVE_ANOC_SNOC				540
+#define SLAVE_LLCC				541
+#define SLAVE_MEM_NOC_SNOC				542
+#define SLAVE_SNOC_MEM_NOC_GC				543
+#define SLAVE_MEM_NOC_PCIE_SNOC				544
+#define SLAVE_IMEM				545
+#define SLAVE_SERVICE_SNOC				546
+#define SLAVE_PCIE_0				547
+#define SLAVE_QDSS_STM				548
+#define SLAVE_TCU				549
+
+#endif
