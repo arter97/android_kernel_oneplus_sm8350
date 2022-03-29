004d42c91f8d248570ef2d5fd06e082b7ec5830f
Shashikala Katthi <quic_skatthi@quicinc.com>
2022-03-29 11:50:32 +0530

    usb: dwc3: gadget: Fix break condition in gadget state
    Add break condition in gadget state of dwc3_gadget_run_stop_util.
    
    Change-Id: I644ae558558616819a7369d8163a68a74c2bb3fd
    Signed-off-by: Shashikala Katthi <quic_skatthi@quicinc.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -2449,6 +2449,7 @@ static int dwc3_gadget_run_stop_util(struct dwc3 *dwc)
 			dwc->gadget_state = DWC3_GADGET_SOFT_CONN;
 			break;
 		}
+		break;
 	case DWC3_GADGET_SOFT_CONN:
 		if (!dwc->softconnect) {
 			dwc->gadget_state = DWC3_GADGET_INACTIVE;
