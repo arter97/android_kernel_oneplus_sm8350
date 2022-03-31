ec8e5c6190c8b12dad0f27080b648232cf689995
Juhyung Park <qkrwngud825@gmail.com>
2022-03-31 22:25:36 +0900

    Revert "f2fs: don't get FREEZE lock in f2fs_evict_inode in frozen fs"
    This reverts commit 99cb4e47f42c3ba9a6ae8358c1f8fc63467caf87.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- fs/f2fs/inode.c
+++ fs/f2fs/inode.c
@@ -778,8 +778,7 @@ void f2fs_evict_inode(struct inode *inode)
 	f2fs_remove_ino_entry(sbi, inode->i_ino, UPDATE_INO);
 	f2fs_remove_ino_entry(sbi, inode->i_ino, FLUSH_INO);
 
-	if (!is_sbi_flag_set(sbi, SBI_IS_FREEZING))
-		sb_start_intwrite(inode->i_sb);
+	sb_start_intwrite(inode->i_sb);
 	set_inode_flag(inode, FI_NO_ALLOC);
 	i_size_write(inode, 0);
 retry:
@@ -810,8 +809,7 @@ retry:
 		if (dquot_initialize_needed(inode))
 			set_sbi_flag(sbi, SBI_QUOTA_NEED_REPAIR);
 	}
-	if (!is_sbi_flag_set(sbi, SBI_IS_FREEZING))
-		sb_end_intwrite(inode->i_sb);
+	sb_end_intwrite(inode->i_sb);
 no_delete:
 	dquot_drop(inode);
 
