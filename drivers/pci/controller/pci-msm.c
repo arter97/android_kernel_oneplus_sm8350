9b9d7058ea9f7b8e7b5dcb30bc27981670871fb9
mafeng <quic_mafeng@quicinc.com>
2022-04-26 13:18:07 +0530

    pci: msm: Initialise work before setting up interrupt handler
    handle_wake_work is scheduled as a part of handling of WAKE irq from EP.
    If WAKE irq is triggered between requesting irq and initializing
    handle_wake_work, it can lead to crashes. This change initializes
    handle_wake_work and set up interrupt handler in the right order.
    
    Change-Id: I323c4248a464d3c296c428915522c9ddaee76d4f
    Signed-off-by: mafeng <quic_mafeng@quicinc.com>

--- drivers/pci/controller/pci-msm.c
+++ drivers/pci/controller/pci-msm.c
@@ -5544,6 +5544,7 @@ static int32_t msm_pcie_irq_init(struct msm_pcie_dev_t *dev)
 
 	/* register handler for PCIE_WAKE_N interrupt line */
 	if (dev->wake_n) {
+		INIT_WORK(&dev->handle_wake_work, handle_wake_func);
 		rc = devm_request_irq(pdev,
 				dev->wake_n, handle_wake_irq,
 				IRQF_TRIGGER_FALLING, "msm_pcie_wake", dev);
@@ -5553,8 +5554,6 @@ static int32_t msm_pcie_irq_init(struct msm_pcie_dev_t *dev)
 				dev->rc_idx);
 			return rc;
 		}
-
-		INIT_WORK(&dev->handle_wake_work, handle_wake_func);
 
 		rc = enable_irq_wake(dev->wake_n);
 		if (rc) {
