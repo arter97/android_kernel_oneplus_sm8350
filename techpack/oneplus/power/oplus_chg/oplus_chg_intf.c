47f287bebb3d796cccc443f0dcd1c0c10ab95481
Albert Tang <tangalbert919@yahoo.com>
2022-08-04 16:42:06 -0500

    oplus_chg: Report full charge capacity properly
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/4791
    Change-Id: I3844ca21fbcc1ef868f685b61fb6a974dceb5273

--- techpack/oneplus/power/oplus_chg/oplus_chg_intf.c
+++ techpack/oneplus/power/oplus_chg/oplus_chg_intf.c
@@ -1481,7 +1481,7 @@ static int oplus_chg_intf_batt_get_prop(struct oplus_chg_mod *ocm, enum oplus_ch
 		pval->intval = chip->batt_fcc * 1000;
 		break;
 	case OPLUS_CHG_PROP_CHARGE_FULL:
-		pval->intval = chip->batt_fcc;
+		pval->intval = chip->batt_fcc * 1000;
 		break;
 	case OPLUS_CHG_PROP_TIME_TO_FULL_AVG:
 		pval->intval = 5000;
