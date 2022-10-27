37f5e8394a22e0907dc0128da8fdd68959de478b
Juhyung Park <qkrwngud825@gmail.com>
2022-10-27 16:41:37 +0900

    mm: multi-gen LRU: merge v12 patchset
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1060,10 +1060,10 @@ static int exec_mmap(struct mm_struct *mm)
 	activate_mm(active_mm, mm);
 	if (IS_ENABLED(CONFIG_ARCH_WANT_IRQS_OFF_ACTIVATE_MM))
 		local_irq_enable();
-	lru_gen_use_mm(mm);
 	tsk->mm->vmacache_seqnum = 0;
 	vmacache_flush(tsk);
 	task_unlock(tsk);
+	lru_gen_use_mm(mm);
 	if (old_mm) {
 		up_read(&old_mm->mmap_sem);
 		BUG_ON(active_mm != old_mm);
