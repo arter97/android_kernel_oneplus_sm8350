9d82d80c8b91dd72f3feb60d86102a1f403aec96
Vishalcj17 <vishalcj@aospa.co>
2023-07-06 04:41:02 +0900

    techpack: display: Don't hack around >1024/1375 - 2047/2753 range.
    Change-Id: I136ba62619fe13570902a0a5f3b0c75b93ae4c34

--- techpack/display/msm/sde/sde_connector.c
+++ techpack/display/msm/sde/sde_connector.c
@@ -129,14 +129,12 @@ static int sde_backlight_device_update_status(struct backlight_device *bd)
 	bl_lvl = mult_frac(brightness, display->panel->bl_config.bl_max_level,
 			display->panel->bl_config.brightness_max_level);
 
-	//Hack to workaround this range having artifacts or being lower brightness
-	//than 1023
 	if (dsi_panel_name == DSI_PANEL_SAMSUNG_AMB670YF01) {
-		if (bl_lvl > 1376 && bl_lvl < 1600)
-			bl_lvl = 1600;
+		if (bl_lvl > 1376)
+			bl_lvl = 2753;
 	} else {
-                if (bl_lvl > 1023 && bl_lvl < 1600)
-                        bl_lvl = 1600;
+                if (bl_lvl > 1023)
+                        bl_lvl = 2047;
 	}
 
 	if (!bl_lvl && brightness)
